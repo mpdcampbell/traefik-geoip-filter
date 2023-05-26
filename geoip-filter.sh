257cbb201a683dfe932ba2332263da7f13dd8af7
mpdcampbell <47434940+mpdcampbell@users.noreply.github.com>
2023-05-26 01:50:31 +0100

    Added functions to write IPList config file

--- geoip-filter.sh
+++ geoip-filter.sh
@@ -3,18 +3,18 @@
 #VARIABLES
 ################
 
-countryCodes=($COUNTRY_CODES) #ISO alpha-2 codes (e.g FR)
-subCodes=($SUB_CODES) #ISO 3166-2 codes (e.g. FR-45)
+countryCodes=($COUNTRY_CODES)
+subCodes=($SUB_CODES)
 maxMindLicenceKey=${MAXMIND_KEY}
-middlewareFilename=${IPWHITELIST_FILENAME:-"geo-ipwhitelist.yml"}
-middlewareName=${IPWHITELIST_NAME:-"middlewares-geo-ipwhitelist"}
-traefikProviderDir=${TRAEFIK_PROVIDER_DIR:-"/rules"}
+filterType="${FILTER_TYPE,,}"
+ipListFilename=${IPLIST_FILENAME:-"IPList.conf"}
 lastModifiedFilename=${LASTMODIFIED_FILENAME:-"LastModified.txt"}
-middlewareFilePath="${traefikProviderDir}/${middlewareFilename}"
+ipListFilePath="/etc/nginx/conf.d/${ipListFilename}"
+defaultConfFilePath="/etc/nginx/conf.d/default.conf"
 lastModifiedDir=${LASTMODIFIED_DIR:-"/geoip"}
 lastModifiedFilePath="${lastModifiedDir}/${lastModifiedFilename}"
-countryDir=${COUNTRY_DIR:-"/geoip/country"}
-subDir=${SUB_DIR:-"/geoip/sub"}
+countryDir=${COUNTRY_DIR:-"${lastModifiedDir}/country"}
+subDir=${SUB_DIR:-"${lastModifiedDir}/sub"}
 countryUrl="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country-CSV&license_key=${maxMindLicenceKey}&suffix=zip"
 subUrl="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City-CSV&license_key=${maxMindLicenceKey}&suffix=zip"
 yearsOldDate="Sun, 07 Jan 1990 01:00:00 GMT"
@@ -63,7 +63,9 @@ sub_getLastModified() {
 } 
 
 country_getZip() {
-  if ! [[ ${remoteLastModified} > ${countryLastModified} ]]; then
+  remoteSeconds=$(date -d "$remoteLastModified" -D "%a, %d %b %Y %T" +'%s')
+  countrySeconds=$(date -d "$countryLastModified" -D "%a, %d %b %Y %T" +'%s')
+  if ! [[ ${remoteSeconds} -gt ${countrySeconds} ]]; then
     echo "Not downloading GeoLite2 Country database as local copy is up to date."
     echo "  Remote GeoLite2 Country database was last updated on ${remoteLastModified}." 
     echo "  Local GeoLite2 Country database version is dated ${countryLastModified}."
@@ -79,13 +81,15 @@ country_getZip() {
       return 1
     else
       echo "${remoteLastModified}" > "${lastModifiedDir}/country${lastModifiedFilename}"
-      country_unzipAndExtractIPs
+      country_unzipAndExtract
     fi
   fi
 }
 
 sub_getZip() {
-  if ! [[ ${remoteLastModified} > ${subLastModified} ]]; then
+  remoteSeconds=$(date -d "$remoteLastModified" -D "%a, %d %b %Y %T" +'%s')
+  subSeconds=$(date -d "$subLastModified" -D "%a, %d %b %Y %T" +'%s')
+  if ! [[ ${remoteSeconds} -gt ${subSeconds} ]]; then
     echo "Not downloading GeoLite2 City database as local copy is up to date."
     echo "  Remote GeoLite2 City database was last updated on ${remoteLastModified}." 
     echo "  Local GeoLite2 City database version is dated ${subLastModified}."
@@ -101,81 +105,101 @@ sub_getZip() {
       exit 1
     else
       echo "${remoteLastModified}" > "${lastModifiedDir}/sub${lastModifiedFilename}"
-      sub_unzipAndExtractIPs
+      sub_unzipAndExtract
     fi
   fi
 }
 
-country_unzipAndExtractIPs() {
+country_unzipAndExtract() {
   unzip -jd ${countryDir} ${countryDir}/country.zip "*Blocks*.csv" "*Country-Locations-en.csv"
   cat ${countryDir}/*Blocks*.csv | cut -d, -f 1-2 > ${countryDir}/globalIPList.txt
-  cat ${countryDir}/*Locations-en.csv | cut -d, -f 1,5,6 > ${countryDir}/countryList.txt
+  cat ${countryDir}/*Locations-en.csv | \
+  cut -d, -f 1,5,6 | \
+  sed -r 's/ /-/g' | \
+  sed -r 's/"//g' > ${countryDir}/countryList.txt
   rm ${countryDir}/country.zip ${countryDir}/*Blocks*.csv ${countryDir}/*Locations-en.csv
 }
 
-sub_unzipAndExtractIPs() {
+sub_unzipAndExtract() {
   unzip -jd ${subDir} ${subDir}/sub.zip "*Blocks*.csv" "*City-Locations-en.csv"
   cat ${subDir}/*Blocks*.csv | cut -d, -f 1-2 > ${subDir}/globalIPList.txt
   cat ${subDir}/*Locations-en.csv | \
-  cut -d, -f 1,5,7,8,9,10 | \
-  sed -r 's/(.*),(.*),(.*),(.*),(.*)(,.*)/\1,\2-\3,\4\,\2-\5\6/' | \
-  sed -r 's/(^.*)(,[A-Z]+-,)(.*)/\1\3/' > ${subDir}/subList.txt
+  cut -d, -f 1,5,6,7,8,9,10,11 | \
+  sed -r 's/ /-/g' | \
+  sed -r 's/"//g' | \
+  sed -r 's/(.*),(.*),(.*),(.*),(.*),(.*),(.*),(.*)/\1,\2-\4,\5\,\2-\6,\7,\8,\3:\8,\5:\8,\7:\8/' | \
+  sed -r 's/(,[A-Z]*-,)//g' | \
+  sed -r 's/(,,[A-Za-z-]*:,.*)//g' | \
+  sed -r 's/(,:.*$)//' > ${subDir}/subList.txt
   rm ${subDir}/sub.zip ${subDir}/*Blocks*.csv ${subDir}/*Locations-en.csv
 }
 
-country_addIPsToMiddleware() {
-  geoNameID=$( grep -hwF "$1" ${countryDir}/countryList.txt | cut -d, -f1 )
+country_addIPsToIPList() {
+  geoNameID=$( grep -hwiF "$1" ${countryDir}/countryList.txt | cut -d, -f1 )
   if [ -z "${geoNameID}" ]; then
     echo "  Country "$1" not found in GeoLite2 Country database, skipping it."
     return 0
   else
-    countryAdded+="$1"
-    echo "  Adding IPs for country "$1" to middleware."
-    echo "          #$1 IPs" >> ${middlewareFilePath}
+    countryAdded+=("$1")
+    echo "  Adding IPs for Country "$1" to ${ipListFilename}."
+    echo "    #$1 IPs" >> ${ipListFilePath}
     printf "%s\n" ${geoNameID[@]} > ${countryDir}/geoNameID.txt
     grep -hwFf ${countryDir}/geoNameID.txt ${countryDir}/globalIPList.txt | \
-    cut -d, -f1 | sed -r 's/(^.*)/          - "\1"/' >> ${middlewareFilePath}
+    cut -d, -f1 | sed -r 's/(^.*)/    "\1" 1;/' >> ${ipListFilePath}
     rm ${countryDir}/geoNameID.txt
   fi
 }
 
-sub_addIPsToMiddleware() {
-  geoNameID=$( grep -hwF "$1" ${subDir}/subList.txt | cut -d, -f1 )
+sub_addIPsToIPList() {
+  geoNameID=$( grep -hwiF "$1" ${subDir}/subList.txt | cut -d, -f1 )
   if [ -z "${geoNameID}" ]; then
     echo "  Location "$1" not found in GeoLite2 City database, skipping it."
     return 0
   else
-    subAdded+="$1"
-    echo "  Adding IPs for Location "$1" to middleware."
-    echo "          #$1 IPs" >> ${middlewareFilePath}
+    subAdded+=("$1")
+    echo "  Adding IPs for Location "$1" to ${ipListFilename}."
+    echo "    #$1 IPs" >> ${ipListFilePath}
     printf "%s\n" ${geoNameID[@]} > ${subDir}/geoNameID.txt
     grep -hwFf ${subDir}/geoNameID.txt ${subDir}/globalIPList.txt | \
-    cut -d, -f1 | sed -r 's/(^.*)/          - "\1"/' >> ${middlewareFilePath}
+    cut -d, -f1 | sed -r 's/(^.*)/    "\1" 1;/' >> ${ipListFilePath}
     rm ${subDir}/geoNameID.txt
   fi
 }
 
-makeEmptyMiddlewareFile() {
-  if [ -f "${middlewareFilePath}" ]; then
-    mv ${middlewareFilePath} ${middlewareFilePath}.old
+writeIpList() {
+  startIpListFile
+  country_loop "${countryCodes[@]}"
+  sub_loop "${subCodes[@]}"
+  endIpListFile
+}
+
+startIpListFile() {
+  if [ -f "${ipListFilePath}" ]; then
+    mv ${ipListFilePath} ${ipListFilePath}.old
   fi
-  echo "Writing new ${middlewareName} middleware."
-cat << EOF > ${middlewareFilePath}
-http:
-  middlewares:
-    ${middlewareName}:
-      ipWhiteList:
-        sourceRange:
+  echo "Writing new ${ipListFilename}"
+cat << EOF > ${ipListFilePath}
+geo \$http_x_forwarded_for \$inIPList {
+    default 0;
+
+EOF
+}
+
+endIpListFile() {
+cat << EOF >> ${ipListFilePath}
+}    
 EOF
 }
 
 insertLocationList() {
-  sed -i "1s/^/\n/" ${middlewareFilePath}
+  sed -i "1s/^/\n/" ${ipListFilePath}
   if ! [ -z "$subAdded" ]; then
-    sed -i "1s/^/#Listed Sublocations: ${subAdded[@]}\n/" ${middlewareFilePath}
+    subString=$(echo "${subAdded[@]}")
+    sed -i "1s/^/#Listed Sublocations: ${subString}\n/" ${ipListFilePath}
   fi
   if ! [ -z "$countryAdded" ]; then
-    sed -i "1s/^/#Listed Countries: ${countryAdded[@]}\n/" ${middlewareFilePath}
+    countryString=$(echo "${countryAdded[@]}")
+    sed -i "1s/^/#Listed Countries: ${countryString}\n/" ${ipListFilePath}
   fi
 }
 
@@ -184,13 +208,13 @@ getZipArray=(country_getZip sub_getZip)
 
 country_loop () {
   for code in "$@"; do
-    country_addIPsToMiddleware $code
+    country_addIPsToIPList $code
   done
 }
 
 sub_loop () {
   for code in "$@"; do
-    sub_addIPsToMiddleware $code
+    sub_addIPsToIPList $code
   done
 }
 
@@ -207,9 +231,6 @@ updateGeoIPDatabase () {
 #Check mandatory variables
 if [ -z "$maxMindLicenceKey" ]; then
   echo "ERROR: The MAXMIND_KEY environment variable is empty, exiting script."
-  exit 1
-elif [ ! -d "$traefikProviderDir" ]; then
-  echo "ERROR: The TRAFEIK_PROVIDER_DIR volume doesn't exist, exiting script."
   exit 1
 fi
 
@@ -228,14 +249,14 @@ fi
 
 if [ ${#codesArray[@]} -gt 0 ]; then
   updateGeoIPDatabase "${codesArray[@]}"
-  makeEmptyMiddlewareFile
-  country_loop "${countryCodes[@]}"
-  sub_loop "${subCodes[@]}"
+  writeIpList
   insertLocationList
-  echo "Middleware completed."
+  echo "${ipListFilename} completed."
 else
   echo "Both COUNTRY_CODES and SUB_CODES environment variables are empty."
-  echo "  No GeoIP locations available to whitelist."
+  echo "  No GeoIP locations available to filter."
   echo "  Exiting script."
   exit 1
 fi
+
+exit 0;
